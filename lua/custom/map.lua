4072d8180c8c61192fc45a69d811be85867dc0cb
Matheus Ferraz <matheus@060.dev>
2023-10-06 15:45:34 -0300

    initial changes

---
+++ lua/custom/map.lua
@@ -0,0 +1,14 @@
+-- Toggle file tree
+vim.keymap.set('n', '<leader>v', ':CHADopen<CR>', { desc = 'Toggle file tree' })
+
+-- LSP 
+-- Format file
+vim.keymap.set('n','<leader>f', ':Format<CR>', {desc = '[F]ormat'})
+
+-- QoL
+-- Keep cursor at center of screen
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
---
+++ lua/custom/set.lua
@@ -0,0 +1,11 @@
+-- set indentation
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Set colorscheme
+vim.o.background = "dark"
+
+-- Make numbers relative
+vim.wo.rnu = true
