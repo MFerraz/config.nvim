a7c4ba9e35283f2cd2eb56760db570579774c6d2
Matheus Ferraz <matheus@060.dev>
2023-10-13 10:15:36 -0300

    feat: add markdown preview

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -33,5 +33,12 @@ return {
         }
       })
     end
-  }
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function() vim.fn["mkdp#util#install"]() end,
+    init = function() vim.g.mkdp_filetypes = { "markdown" } end,
+    ft = { "markdown" },
+  },
 }
