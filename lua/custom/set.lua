b6a374598c14aa4d7f3a8649b7de83b8eb123caa
Matheus Ferraz <matheus@060.dev>
2023-11-07 18:33:40 -0300

    chore: shrink tabs

--- lua/custom/set.lua
+++ lua/custom/set.lua
@@ -1,8 +1,9 @@
 -- set indentation
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
+vim.opt.autoindent = true
 
 -- Set colorscheme
 vim.o.background = "dark"
