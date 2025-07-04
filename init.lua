3e2e985be6ea509946a4ba5e52f1cb698971d76e
Matheus Ferraz <matheus@060.dev>
2025-07-03 21:33:26 -0300

    fix: which-key syntax

--- init.lua
+++ init.lua
@@ -405,14 +405,19 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register({
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+require('which-key').add({
+  { '<liader>c', group = '[C]ode' },
+  { '<leader>c_', hidden = true },
+  -- { '<leader>d', group = '[D]ocument' },
+  -- { '<leader>d_', hidden = true },
+  { '<leader>h', group = 'More git' },
+  { '<leader>h_', hidden = true },
+  { '<leader>r', group = '[R]ename' },
+  { '<leader>r_', hidden = true },
+  { '<leader>s', group = '[S]earch' },
+  { '<leader>s_', hidden = true },
+  { '<leader>w', group = '[W]orkspace' },
+  { '<leader>w_', hidden = true },
 })
 
 -- Enable the following language servers
@@ -519,4 +524,8 @@ require("custom.map")
 require("custom.set")
 
 -- Setup custom plugins
-require("oil").setup()
+require("oil").setup({
+  view_options = {
+    show_hidden = true,
+  },
+})
